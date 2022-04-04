3681522f4883a0179c565754a56949ac84906c9b
ABHIRAMI P S <psabhirami015@gmail.com>
2022-04-04 21:00:46 +0530

    filter and sort updates

--- routes/UniversityDetails/getUniversityDetails.js
+++ routes/UniversityDetails/getUniversityDetails.js
@@ -2,13 +2,68 @@ const { University } = require("../../models/universityModel");
 const express = require("express");
 const router = express.Router();
 
+const caseInsensitiveCheck = (string) => {
+  return new RegExp([string].join(""), "i");
+};
 router.get("/", async (req, res) => {
-  let university = await University.find();
+  try {
+    let university;
 
-  res.status(200).send({
-    message: "University fetched successfully",
-    university: university,
-  });
+    if (req.query.location && !req.query.course) {
+      const location = await caseInsensitiveCheck(req.query.location);
+      university = await University.find({ location: location }).sort(
+        req.query.sortOrder == "DESC"
+          ? {
+              university_name: -1,
+            }
+          : { university_name: 1 }
+      );
+    } else if (req.query.course && !req.query.location) {
+      const course = await caseInsensitiveCheck(req.query.course);
+      university = await University.find({ course: course }).sort(
+        req.query.sortOrder == "DESC"
+          ? {
+              university_name: -1,
+            }
+          : { university_name: 1 }
+      );
+    } else if (req.query.course && req.query.location) {
+      const location = await caseInsensitiveCheck(req.query.location);
+      const course = await caseInsensitiveCheck(req.query.course);
+      university = await University.find({
+        course: course,
+        location: location,
+      }).sort(
+        req.query.sortOrder == "DESC"
+          ? {
+              university_name: -1,
+            }
+          : { university_name: 1 }
+      );
+    } else {
+      university = await University.find().sort(
+        req.query.sortOrder == "DESC"
+          ? {
+              university_name: -1,
+            }
+          : { university_name: 1 }
+      );
+    }
+
+    if (university.length) {
+      return res.status(200).send({
+        message: "University fetched successfully",
+        university: university,
+      });
+    }
+    return res.status(400).send({
+      message: "No Universities found",
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Internal Server Error",
+    });
+  }
 });
 
 module.exports = router;
